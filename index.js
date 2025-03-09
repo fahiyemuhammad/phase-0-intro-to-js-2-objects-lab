990d29abd8bb875200962bbe20bdd984554ede9c
Fahiye Muhammad <fahiye254@gmail.com>
2025-03-09 16:00:46 +0300

    Manipulating objects

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name:"Sam",
+    streetAddrees:"11 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return {
+        ...employee,[key]:value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key] = value
+    return obj
+}
+
+function deleteFromEmployeeByKey(employee,key){
+const newEmployee = {...employee}
+    
+       delete newEmployee[key]
+       return newEmployee
+    
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
+
+
